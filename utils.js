5f3ea255c08cbc241672adf9bf52c1c96ce207e6
thrilling_gem <4096void@gmail.com>
2019-04-19 14:46:24 +0800

    refactor tt: 1. to traverse any specific layer

--- utils.js
+++ utils.js
@@ -1,35 +1,36 @@
 /**
- * A really really naive tree depth-first traverser.
+ * Traverse nodes of a tree with specific depth, with { `nodeName`: `nodeValue` }.
  * @constructor
  * @param {object} tree - a simple nested tree.
+ * @param {number} d - specific depth, default to `Infinity`(get all the leaf nodes).
+ * @param {string} t - the name of root, default to `treename`.
  *
- * var test = {
- *  name1: {
- *    name2: {
- *      name3: {
- *        name4: 'value4',
- *      },
- *    },
- *    name5: {
- *      name6: 'value6',
- *    },
- *    name7: {
- *      name8: 'value8',
- *    },
- *  },
- * };
- *
- * tt(test); // [...4 ...6 ...8]
  */
-const tt = tree => {
+const tt = (
+  tree,
+  d = Infinity,
+  t = 'treeName',
+) => {
   let rs = [];
+
+  /* Root is a single object. */
+  if (d === 0) {
+    let item = {};
+    item[t] = tree;
+    return [item];
+  }
+  // TODO: root is an array.
+
   for (k in tree) {
-    if (typeof tree[k] === 'object') {
-      rs.push(...tt(tree[k]));
-    } else {
+    if (d === 0) {
       let item = {};
       item[k] = tree[k];
       rs.push(item);
+    } else if (d > 0
+      && typeof tree[k] === 'object') {
+      rs.push(...tt(tree[k], d));
+    } else {
+      rs.push(tree[k]);
     }
   }
 
